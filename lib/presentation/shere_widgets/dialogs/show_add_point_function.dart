209e3c4a6e417a88146c283055d6eafe66a5894f
sefeen <sefeen>
2022-12-02 23:23:08 +0200

    add dialog

---
+++ lib/presentation/shere_widgets/dialogs/show_add_point_function.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:schools/presentation/shere_widgets/dialogs/add_point_dialog_widget.dart';
+
+void showAddPointFunction(
+        {required BuildContext? context, required Function() addAction,required TextEditingController addController}) =>
+    showDialog(
+        barrierDismissible: true,
+        context: context!,
+        builder: (BuildContext context) => Dialog(
+            elevation: 0,
+            backgroundColor: Colors.transparent,
+            child: AddPointDialogWidget(
+              addAction: addAction, addController: addController,
+            )));
--- lib/presentation/ui/home/widgets/home_list_widget.dart
+++ lib/presentation/ui/home/widgets/home_list_widget.dart
@@ -1,12 +1,14 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:schools/core/utils/resorces/image_path.dart';
 import 'package:schools/presentation/shere_widgets/card_widget.dart';
+import 'package:schools/presentation/shere_widgets/dialogs/show_add_point_function.dart';
 
 class HomeListWidget extends StatelessWidget {
   const HomeListWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final TextEditingController addController = TextEditingController();
     return Expanded(
       child: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -16,12 +18,20 @@ class HomeListWidget extends StatelessWidget {
         shrinkWrap: false,
         semanticChildCount: 10,
         itemBuilder: (BuildContext context, int index) {
-          return const Padding(
-            padding: EdgeInsets.all(5),
-            child: CardWidget(
-              section: "Section A",
-              imagePath: ImagesPath.schoolItem,
-              grade: "Grade 2",
+          return Padding(
+            padding: const EdgeInsets.all(5),
+            child: InkWell(
+              onTap: () => showAddPointFunction(
+                  context: context,
+                  addAction: () {
+                    Navigator.pop(context);
+                  },
+                  addController: addController),
+              child: const CardWidget(
+                section: "Section A",
+                imagePath: ImagesPath.schoolItem,
+                grade: "Grade 2",
+              ),
             ),
           );
         },
