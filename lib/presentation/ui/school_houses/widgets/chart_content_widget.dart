f0111f077a4c5837eea8f61cfb04e22c12adde6a
sefeen <sefeen>
2022-12-06 18:48:30 +0200

    handle some design

--- lib/presentation/ui/school_houses/widgets/chart_content_widget.dart
+++ lib/presentation/ui/school_houses/widgets/chart_content_widget.dart
@@ -12,27 +12,27 @@ class _ChartContentWidgetState extends State<ChartContentWidget> {
 
 
     List<SalesData> columnData=[
-      SalesData(x: "hello 1",y: 1.0),
-      SalesData(x: "hello 2",y: 1.0),
-      SalesData(x: "hello 3",y: 1.0),
-      SalesData(x: "hello 4",y: 1.0),
+      SalesData(x: "hello 1",y: 170),
+      SalesData(x: "hello 2",y: 170),
+      SalesData(x: "hello 3",y: 170),
+      SalesData(x: "hello 4",y: 170),
     ];
 
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-        padding: const EdgeInsets.all(50),
-        child: SfCartesianChart(
+    return   SfCartesianChart(
           primaryXAxis: CategoryAxis(
             borderColor: Colors.white,
             borderWidth: 0,
             isVisible: true,
+              labelStyle: const TextStyle(color: Colors.transparent),
               majorGridLines:const MajorGridLines(color: Colors.transparent),
             axisLine:const  AxisLine(color: Colors.white),
 
           ),
           primaryYAxis: NumericAxis(
+              labelStyle: const TextStyle(color: Colors.white),
             isVisible: true,
             majorGridLines:const MajorGridLines(color: Colors.transparent),
             axisLine:const  AxisLine(color: Colors.white),
@@ -44,17 +44,21 @@ class _ChartContentWidgetState extends State<ChartContentWidget> {
           borderColor: Colors.transparent,
           borderWidth: 0,
           legend: Legend(isVisible: false),
-
           plotAreaBorderColor: Colors.transparent,
           series: <ChartSeries> [
             ColumnSeries<SalesData, String>(
               dataSource: columnData,
               xValueMapper: (SalesData salesData,_)=>salesData.x,
               yValueMapper: (SalesData salesData,_)=>salesData.y,
-            )
+              color: Colors.white,
+              trackColor: Colors.transparent,
+              isTrackVisible: true,
+              dataLabelSettings:const  DataLabelSettings(color: Colors.white),
+              isVisibleInLegend: false,
+              spacing: 1.5
+            ),
           ],
-
-        )
+      margin: const EdgeInsets.only(top: 80,left: 50,right: 50),
     );
 
 }
