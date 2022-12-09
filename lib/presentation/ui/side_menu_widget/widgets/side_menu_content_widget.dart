1e06185651451852fc345c75dee30cccb54b4199
sefeen <sefeen>
2022-12-09 10:13:55 +0200

    handle design

--- lib/presentation/ui/side_menu_widget/widgets/side_menu_content_widget.dart
+++ lib/presentation/ui/side_menu_widget/widgets/side_menu_content_widget.dart
@@ -17,62 +17,51 @@ class SideMenuContentWidget extends StatefulWidget {
 class _SideMenuContentWidgetState extends State<SideMenuContentWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.only(
-              topRight: Radius.elliptical(900.0, 2000.0),
-              bottomRight: Radius.elliptical(900.0, 2000.0))
-      ),
-      child: Column(
-        children: [
-          const SideMenuHeader(),
-          Expanded(
-              child: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  ColorsManager.primaryColor,
-                  ColorsManager.secondaryColor,
-                ],
-                stops: [0.5, 0.8],
+    return Column(
+      children: [
+        const SideMenuHeader(),
+        Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      ColorsManager.primaryColor,
+                      ColorsManager.secondaryColor,
+                    ],
+                    stops: [0.5, 0.8],
+                  )),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Column(
+                  children: [
+                    SideMenuItem(
+                        icon: Icons.home_filled,
+                        title: "School Homes",
+                        onTap: () => BlocProvider.of<SideMenuBloc>(context)
+                            .add(SideMenuHomeEvent())),
+                    _line(),
+                    SideMenuItem(
+                        icon: Icons.settings,
+                        title: "Settings",
+                        onTap: () => BlocProvider.of<SideMenuBloc>(context)
+                            .add(SideMenuSettingsEvent())),
+                    _line(),
+                    SideMenuItem(
+                        icon: Icons.mail,
+                        title: "Contact Us",
+                        onTap: () => BlocProvider.of<SideMenuBloc>(context)
+                            .add(SideMenuContactUsEvent())),
+                    _line(),
+                    SideMenuItem(
+                        icon: Icons.info_outline,
+                        title: "About App",
+                        onTap: () => BlocProvider.of<SideMenuBloc>(context)
+                            .add(SideMenuAboutAppEvent())),
+                  ],
+                ),
               ),
-              borderRadius: BorderRadius.only(
-                  topRight: Radius.elliptical(15.0, 200.0),
-                  bottomRight: Radius.elliptical(200.0, 400.0)),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Column(
-                children: [
-                  SideMenuItem(
-                      icon: Icons.home_filled,
-                      title: "School Homes",
-                      onTap: () => BlocProvider.of<SideMenuBloc>(context)
-                          .add(SideMenuHomeEvent())),
-                  _line(),
-                  SideMenuItem(
-                      icon: Icons.settings,
-                      title: "Settings",
-                      onTap: () => BlocProvider.of<SideMenuBloc>(context)
-                          .add(SideMenuSettingsEvent())),
-                  _line(),
-                  SideMenuItem(
-                      icon: Icons.mail,
-                      title: "Contact Us",
-                      onTap: () => BlocProvider.of<SideMenuBloc>(context)
-                          .add(SideMenuContactUsEvent())),
-                  _line(),
-                  SideMenuItem(
-                      icon: Icons.info_outline,
-                      title: "About App",
-                      onTap: () => BlocProvider.of<SideMenuBloc>(context)
-                          .add(SideMenuAboutAppEvent())),
-                ],
-              ),
-            ),
-          )),
-        ],
-      ),
+            )),
+      ],
     );
   }
 
