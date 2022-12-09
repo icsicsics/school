7ee73aa9446b2c69f058419c931b69cff3fe4399
sefeen <sefeen>
2022-12-09 10:47:28 +0200

    add network handler

---
+++ lib/core/utils/network/interceptor.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:dio/dio.dart';
+
+class CustomInterceptors extends InterceptorsWrapper {
+  @override
+  void onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    debugPrint(
+        "REQUEST [${' '}${options.method}${' '}] ${' '} URL: ${options.baseUrl + options.path} ${jsonEncode(options.data)}");
+    return super.onRequest(options, handler);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) async {
+    debugPrint(
+        "RESPONSE [${' '}${response.statusCode}${' '}]${'\n'}${jsonEncode(response.data)}");
+    return super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) async {
+    debugPrint(
+        "ERROR [${' '}${err.response?.statusCode}${' '}]${'\n'} error meesage =>"
+        " ${err.response.toString()} ${'\n'} error header => ");
+    return super.onError(err, handler);
+  }
+}
