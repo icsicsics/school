2f835c5281dd87c74fb6b7b80dd496c8f02bc102
Hassan <hassansobhy60@gmail.com>
2023-03-29 21:18:26 +0200

    fix chart

--- lib/presentation/screens/school_houses/widgets/chart_content_widget.dart
+++ lib/presentation/screens/school_houses/widgets/chart_content_widget.dart
@@ -22,20 +22,17 @@ class _ChartContentWidgetState extends State<ChartContentWidget> {
   @override
   void initState() {
     super.initState();
-    if(widget.schoolHousesBloc.getClassHousesResponse.data!.isNotEmpty){
-      for(var item in widget.schoolHousesBloc.getClassHousesResponse.data!) {
+    if (widget.schoolHousesBloc.getClassHousesResponse.data!.isNotEmpty) {
+      for (var item in widget.schoolHousesBloc.getClassHousesResponse.data!) {
         columnData.add(SalesData(
-            x: item.houseName ?? "",
-            y: item
-                .totalPointsHouse!
-                .toDouble()));
+            x: item.houseName ?? "", y: item.totalPointsHouse!.toDouble()));
       }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-      return buildScreenContent(columnData);
+    return buildScreenContent(columnData);
   }
 
   Align buildScreenContent(List<SalesData> columnData) {
@@ -45,6 +42,7 @@ class _ChartContentWidgetState extends State<ChartContentWidget> {
             padding: const EdgeInsets.only(top: 150),
             child: SfCartesianChart(
               primaryXAxis: CategoryAxis(
+
                 borderColor: Colors.white,
                 borderWidth: 0,
                 isVisible: true,
@@ -55,28 +53,50 @@ class _ChartContentWidgetState extends State<ChartContentWidget> {
                   labelStyle: const TextStyle(color: Colors.white),
                   isVisible: true,
                   majorGridLines:
-                  const MajorGridLines(color: Colors.transparent),
+                      const MajorGridLines(color: Colors.transparent),
                   axisLine: const AxisLine(color: Colors.white),
                   minorGridLines:
-                  const MinorGridLines(color: Colors.transparent),
+                      const MinorGridLines(color: Colors.transparent),
                   borderColor: Colors.transparent,
                   borderWidth: 0,
                   placeLabelsNearAxisLine: false),
               borderColor: Colors.transparent,
               borderWidth: 0,
-              legend: Legend(isVisible: false),
+              legend: Legend(
+                  isVisible: true,
+                  alignment: ChartAlignment.center,
+                  overflowMode: LegendItemOverflowMode.wrap,
+                  position: LegendPosition.bottom,
+                  // Templating the legend item
+                  legendItemBuilder:
+                      (String name, dynamic series, dynamic point, int index) {
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: widget.schoolHousesBloc.getClassHousesResponse
+                              .data!.isNotEmpty
+                          ? widget.schoolHousesBloc.getClassHousesResponse.data!
+                              .map((e) => FaIcon(getIconFromCss(
+                                    e.houseIcon ?? "",
+                                  )))
+                              .toList()
+                          : [],
+                    );
+                  }),
               plotAreaBorderColor: Colors.transparent,
               series: <ChartSeries>[
                 ColumnSeries<SalesData, String>(
                     dataSource: columnData,
-                    xValueMapper: (SalesData salesData, _) => salesData.x,
+                    xValueMapper: (SalesData salesData, _) {
+                      return salesData.x;
+                    },
                     yValueMapper: (SalesData salesData, _) => salesData.y,
                     color: Colors.white,
                     trackColor: Colors.transparent,
                     isTrackVisible: true,
+                    legendIconType: LegendIconType.image,
                     dataLabelSettings:
-                    const DataLabelSettings(color: Colors.white),
-                    isVisibleInLegend: false,
+                        const DataLabelSettings(color: Colors.white),
+                    isVisibleInLegend: true,
                     spacing: 1.5),
               ],
               margin: const EdgeInsets.only(
@@ -86,7 +106,10 @@ class _ChartContentWidgetState extends State<ChartContentWidget> {
 }
 
 class SalesData {
-  SalesData({required this.x, required this.y});
+  SalesData({
+    required this.x,
+    required this.y,
+  });
 
   final String x;
   final double y;
